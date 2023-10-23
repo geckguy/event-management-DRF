54574b7a1dac70b9f26aa39a4b686d09ec895c76
geckguy <gamincon4112003@gmail.com>
2023-10-23 12:53:28 +0530

    add register serializer

--- eventmanagement/api/serializers.py
+++ eventmanagement/api/serializers.py
@@ -9,14 +9,27 @@ class CollegeSerializer(serializers.ModelSerializer):
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
-        fields = '__all__'
+        fields = ('id', 'user', 'name', 'roll_number', 'college')
 
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
-        fields = '__all__'
+        fields = ('id', 'title', 'date', 'college')
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ('id', 'email', 'name', 'college', 'roll_number')
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password', 'name', 'college')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = CustomUser(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
--- eventmanagement/api/views.py
+++ eventmanagement/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.hashers import make_password
 from .models import College, Student, Event, CustomUser
-from .serializers import CollegeSerializer, StudentSerializer, EventSerializer, CustomUserSerializer
+from .serializers import CollegeSerializer, StudentSerializer, EventSerializer, CustomUserSerializer, RegisterSerializer
 from .permissions import IsSameCollege
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -21,33 +21,42 @@ class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+# class RegisterView(APIView):
+#     def post(self, request):
+#         data = request.data
+#         email = data.get('email')
+#         password = data.get('password')
+#         name = data.get('name')
+#         college_id = data.get('college')
+#         roll_number = data.get('roll_number')
+#         if not (email and password and name and college_id and roll_number):
+#             logger.error('All fields are required for registration')
+#             return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+#         try:
+#             college = College.objects.get(id=college_id)
+#         except ObjectDoesNotExist:
+#             logger.error(f'College with id {college_id} does not exist')
+#             return Response({'error': 'Invalid college ID'}, status=status.HTTP_400_BAD_REQUEST)
+#         try:
+#             if Student.objects.filter(roll_number=roll_number).exists():
+#                 logger.error(f'Student with roll number {roll_number} already exists')
+#                 return Response({'error': 'Student with this roll number already exists'}, status=status.HTTP_400_BAD_REQUEST)
+#             user = CustomUser.objects.create(email=email, password=make_password(password), name=name, college=college)
+#             Student.objects.create(user=user, name=name, college=college, roll_number=roll_number)
+#         except IntegrityError:
+#             logger.error(f'User with email {email} already exists')
+#             return Response({'error': 'User with this email already exists'}, status=status.HTTP_400_BAD_REQUEST)
+#         logger.info(f'User registered successfully with email {email}')
+#         return Response(CustomUserSerializer(user).data)
+
 class RegisterView(APIView):
     def post(self, request):
-        data = request.data
-        email = data.get('email')
-        password = data.get('password')
-        name = data.get('name')
-        college_id = data.get('college')
-        roll_number = data.get('roll_number')
-        if not (email and password and name and college_id and roll_number):
-            logger.error('All fields are required for registration')
-            return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            college = College.objects.get(id=college_id)
-        except ObjectDoesNotExist:
-            logger.error(f'College with id {college_id} does not exist')
-            return Response({'error': 'Invalid college ID'}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            if Student.objects.filter(roll_number=roll_number).exists():
-                logger.error(f'Student with roll number {roll_number} already exists')
-                return Response({'error': 'Student with this roll number already exists'}, status=status.HTTP_400_BAD_REQUEST)
-            user = CustomUser.objects.create(email=email, password=make_password(password), name=name, college=college)
-            Student.objects.create(user=user, name=name, college=college, roll_number=roll_number)
-        except IntegrityError:
-            logger.error(f'User with email {email} already exists')
-            return Response({'error': 'User with this email already exists'}, status=status.HTTP_400_BAD_REQUEST)
-        logger.info(f'User registered successfully with email {email}')
-        return Response(CustomUserSerializer(user).data)
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(CustomUserSerializer(user).data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
